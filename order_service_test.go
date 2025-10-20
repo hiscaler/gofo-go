0b1ac5695cb3d2041ddef4fbfe7a90bc5c9f565e
hiscaler <hiscaler@gmail.com>
2025-10-20 17:19:39 +0800

    ...

--- order_service_test.go
+++ order_service_test.go
@@ -4,15 +4,14 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/hiscaler/gofo-go/entity"
 	"gopkg.in/guregu/null.v4"
 )
 
 func TestOrderService_Create(t *testing.T) {
-	req := entity.CreateOrderRequest{
+	req := CreateOrderRequest{
 		COrderNo:      null.StringFrom("TEST_ORDER_001"),
 		DeclaredValue: 12,
-		OrderShipper: entity.OrderShipper{
+		OrderShipper: OrderShipper{
 			ShipperName:    "test",
 			ShipperPhone:   "13000000000",
 			ShipperCountry: "CN",
@@ -22,7 +21,7 @@ func TestOrderService_Create(t *testing.T) {
 			ShipperCode:    "90058",
 		},
 		ProductCode: null.StringFrom("GOFO Parcel Pickup"),
-		OrderConsignee: entity.OrderConsignee{
+		OrderConsignee: OrderConsignee{
 			ConsigneeName:    "test",
 			ConsigneePhone:   "13000000000",
 			ConsigneeCountry: "US",
@@ -31,13 +30,13 @@ func TestOrderService_Create(t *testing.T) {
 			Address1:         "test address",
 			ConsigneeCode:    "90001",
 		},
-		OrderGoods: entity.OrderGoods{
+		OrderGoods: OrderGoods{
 			Weight: 1,
 			Length: 1,
 			Height: 1,
 			Width:  1,
 		},
-		OrderItemList: []entity.OrderItem{
+		OrderItemList: []OrderItem{
 			{
 				ItemNameEn: "test",
 				ItemNameZh: "测试",
@@ -54,8 +53,8 @@ func TestOrderService_Create(t *testing.T) {
 }
 
 func TestOrderService_Cancel(t *testing.T) {
-	req := entity.CancelOrderRequest{
-		OrderNo: "WAYBILL_001",
+	req := CancelOrderRequest{
+		OrderNo: "GFUS01014625997824",
 	}
 	_, err := client.Services.Order.Cancel(ctx, req)
 	if err != nil {
@@ -64,14 +63,14 @@ func TestOrderService_Cancel(t *testing.T) {
 }
 
 func TestOrderService_ShippingLabel(t *testing.T) {
-	_, err := client.Services.Order.ShippingLabel(ctx, "WAYBILL_001")
+	_, err := client.Services.Order.ShippingLabel(ctx, "GFUS01014625997824")
 	if err != nil {
 		t.Errorf("Expected no error, got %v", err)
 	}
 }
 
 func TestOrderService_Track(t *testing.T) {
-	_, err := client.Services.Order.Track(ctx, "WAYBILL_001")
+	_, err := client.Services.Order.Track(ctx, "GFUS01014625997824")
 	if err != nil {
 		t.Errorf("Expected no error, got %v", err)
 	}
